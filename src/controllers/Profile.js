b86002ef155c787794b80229b5c50ace10c19636
crpistillo <cpistillo@fi.uba.ar>
2020-06-21 18:20:07 -0300

    Apuntes

--- src/controllers/Profile.js
+++ src/controllers/Profile.js
@@ -1,37 +1,54 @@
 import React, {Component} from 'react';
 
+//TODO: Para poder realizar una llamada a una API
+
 
 export class Profile extends Component {
     constructor(props) {
-        super(props);
+        super(props); //TODO: Si definimos el constructor lo primero que hay que hacer es llamar a esto
+                      //TODO: se lllama a la clase "Component" con las props
 
         this.state = {
             userData: {}
         };
 
+        //TODO: Binding en el constructor.
         this.handleApiResponse = this.handleApiResponse.bind(this);
     }
 
+    //TODO: En general para hacer llamadas a una API (en la carga de una url) lo que mas se usa es el componentDidMount
+    //TODO: es el ultimo paso del montado de un componente
     componentDidMount() {
         this.getUserData();
     }
 
+    //TODO: El render es la unica funcion obligatoria a definir dentro del ciclo de vida de un componente
+    //TODO: ni siquiera el constructor es esencial
     render() {
         return (
             <div>
                 <h2>Perfil del usuario</h2>
-                <p>Email: {this.state.userData['first_name']}</p>
-                <p>Nombre: {this.state.userData['last_name']}</p>
-                <p>Apellido: {this.state.userData['email']}</p>
+                <p>Email: {this.state.userData['email']}</p>
+                <p>Nombre: {this.state.userData['first_name']}</p>
+                <p>Apellido: {this.state.userData['last_name']}</p>
             </div>
         )
     }
 
+    //TODO: Seteo el state con el json obtenido en la response para que se muestre el resultado en el render
+    //TODO: Nombre, apellido, email (ver el render de arriba)
     handleApiResponse(userJson) {
         this.setState({userData: userJson.data})
     }
 
+    //TODO: esto hace un fetch a la url que mi api publica indica que me da un usuario
     getUserData() {
+        //TODO: fetch es una funcion que da la api request de JS que devuelve una Promise (asincronico)
+        //TODO: con una response http. El .json de la response tambien devuelve Promise
+        //TODO: then recibe una funcion: callback
+
         fetch("https://reqres.in/api/users/2").then(response => response.json()).then(this.handleApiResponse);
+        //TODO: el 'this' de aca no es el componente (la clase), sino el this dentro de este scope/contexto de
+        //TODO: esta funcion, sino del modulo que la este ejecutando
     }
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,13 +6,14 @@ import {Profile} from "../controllers/Profile";
 class Routes extends Component {
     render() {
         return (
+            //TODO: aca dentro puede haber tantas rutas como queramos
+            //TODO: cada componente routeado aca matchea con un controller
             <Router key="router">
                 <Route exact path={"/home"} component={Home}/>
                 <Route exact path={"/profile"} component={Profile}/>
             </Router>
         )
     }
-
 
 }
 
